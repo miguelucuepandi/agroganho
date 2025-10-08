0c1a8ba9ec26ddd609799a36bde8d5cf3b69c44d
miguelucuepandi <miguel.ucuepandi@gmail.com>
2025-10-08 23:10:35 +0100

    Meti o modo dark/light da página home

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,13 @@
 const btnMenu = document.getElementById('btnMenu');
 const sideBar = document.querySelector('section#left');
+const btnTema = document.querySelector('#btnTema');
+let body = document.querySelector("body")
 
 btnMenu.addEventListener('click', () => {
     btnMenu.classList.toggle('ativar');
     sideBar.classList.toggle('abrir');
+})
+
+btnTema.addEventListener('click', () => {
+    body.classList.toggle('dark');
 })
